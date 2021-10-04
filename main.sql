56f4362327c0ed7ba3bdc0d80e3dc0ae91b3b46f
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-04 21:44:42 +0000

    fix 'name' ambiguous - final

--- main.sql
+++ main.sql
@@ -2,4 +2,4 @@
 Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 */
-select name from city inner join country on CITY.CountryCode=COUNTRY.Code where continent='Africa'
+select CITY.name from city inner join country on CITY.CountryCode=COUNTRY.Code where continent='Africa'
